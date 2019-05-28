bbd6b6bb8527be7000a3894efe74f03b601b760e
filmscore88 <cmtmorales24@gmail.com>
2019-05-28 14:29:45 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,5 @@
 class ArtistsController < ApplicationController
+  before_action :set_preferences, only: [:new]
   def index
     @artists = Artist.all
   end
@@ -8,7 +9,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+
+    if @preference.allow_create_artists == false
+      redirect_to artists_path
+    else
+      @artist = Artist.new
+    end
   end
 
   def create
@@ -45,6 +51,10 @@ class ArtistsController < ApplicationController
   end
 
   private
+
+  def set_preferences
+    @preference= Preference.first
+  end
 
   def artist_params
     params.require(:artist).permit(:name)
